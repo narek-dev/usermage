843c72c3f3f174a8eaa43cd51a2f08be1a2a8c46
Narek Yazeryan <narek.yazeryan@esterox.am>
2021-08-16 17:46:47 +0400

    Removing Redux Devtools support

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,7 @@ import rootReducer from "./reducers";
 export const store = createStore(
   rootReducer,
   compose( 
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
   )
 );
 
